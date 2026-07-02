8290b2ad092aae00d498d6559f7e785afe9aeba5
venkatapadma <padmaganapathi568@gmail.com>
2026-07-02 14:10:52 +0100

    resolved build issues-1

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,6 +1,8 @@
 import clsx from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export const cn = (...inputs) => {
+export const cn = (...inputs:classNameValue[]):String => {
   return twMerge(clsx(inputs));
 }
+
+type classNameValue = string| number| boolean | null | undefined | classNameValue[] | {[key:string]:unknown}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path, { dirname } from "path"
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
